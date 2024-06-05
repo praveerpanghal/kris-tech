ce8e96ebd48eb2b0b407319836ff61be8ccfaf61
Praveer Panghal <admin@spinoinc.com>
2024-06-05 07:36:56 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
+import "./plugins/bootstrap/bootstrap.min.css";
+import "./plugins/Ionicons/css/ionicons.min.css";
+import "./plugins/animate-css/animate.css";
+import "./plugins/magnific-popup/magnific-popup.css";
+import "./plugins/slick/slick.css";
+import "./scss/style.scss"
 
-
-import "./plugins/bootstrap/bootstrap.min.css"
-
-import "./plugins/Ionicons/css/ionicons.min.css"
-
-import "./plugins/animate-css/animate.css"
-
-import "./plugins/magnific-popup/magnific-popup.css"
-
-import "./plugins/slick/slick.css"
-// import "./scss/style.scss"
 import Header from './components/header/header';
 
 function App() {
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
