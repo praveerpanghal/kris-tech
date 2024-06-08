c3c52f1ebb3c2eb5f45b9c3359681daf4262c44c
Praveer Panghal <admin@spinoinc.com>
2024-06-07 22:25:05 -0400

    .

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,12 +1,13 @@
 import About from '../about/about';
 import Header from '../header/header';
 import Slider from '../slider/slider';
-const Home =()=>{
-    return(<div>
-         <Header />
-         <Slider/>
-         <About/>
-
-    </div>)
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Slider />
+            <About />
+        </div>
+    )
 }
 export default Home;
